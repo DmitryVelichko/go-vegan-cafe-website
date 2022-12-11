24622f2ee680117e0300b02fbe4655aab9ca90b1
Dmitry Velichko <soleil-2005@mail.ru>
2022-12-11 12:44:04 +0400

    Fixes

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
       <SubHeading title="С любовью к планете и животным" />
-      <h1 className="app__header-h1">Кафе растительной кухни</h1>
+      <h1 className="app__header-h1">Кафе <br />растительной кухни</h1>
       <p className="p__opensans" style={{ margin: '2rem 0' }}>Готовим вкусную, полезную и этическую еду ❤️</p>
       <a href={menu} target="_blank" rel="noreferrer"><button type="button" className="custom__button">Смотреть меню</button></a>
     </div>
